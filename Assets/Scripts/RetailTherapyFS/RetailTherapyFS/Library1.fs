93564ce26a886f9d52fe77b4f24b0bfc29f5d7d1
Matthew Traylor <mhtraylor@hotmail.com>
2015-04-18 12:43:45 -0500

    Initial commit of directory structure and FSharp project.

---
+++ Assets/Scripts/RetailTherapyFS/RetailTherapyFS/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace RetailTherapyFS
+
+type Class1() = 
+    member this.X = "F#"
---
+++ Assets/Scripts/RetailTherapyFS/RetailTherapyFS/Player.fs
@@ -0,0 +1,10 @@
+﻿namespace RetailTherapy.Player
+
+open System
+open UnityEngine
+
+type Test() =
+    inherit MonoBehaviour()
+
+    member x.Awake() = Debug.Log("I'm awake!")
+
